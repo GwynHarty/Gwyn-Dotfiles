12f741254a850ce2501693e42a77687de25345ea
Gustavo B. <gustavobs772@gmail.com>
2025-08-06 14:32:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-set -e
+set -e  # Encerra se qualquer comando falhar
+set -u  # Encerra se tentar usar variáveis não definidas
+set -o pipefail  # Garante que erros em pipes sejam tratados corretamente
 
-if [ "$EUID" -ne 0 ]; then
-  echo "Execute como root: sudo $0"
-  exit 1
+# Instalação do PipeWire e pacotes multimídia
+sudo pacman -S --noconfirm \
+    pipewire \
+    pipewire-alsa \
+    pipewire-jack \
+    pipewire-pulse \
+    wireplumber \
+    gstreamer \
+    gst-libav \
+    gst-plugins-base \
+    gst-plugins-good \
+    gst-plugins-bad \
+    gst-plugins-ugly \
+    ffmpeg
+    
+# Instalar o Git (caso ainda não esteja instalado)
+sudo pacman -S --needed --noconfirm git base-devel
+
+# Clonar e instalar o yay (AUR helper)
+if [ ! -d "$HOME/yay" ]; then
+    git clone https://aur.archlinux.org/yay.git ~/yay
+    cd ~/yay
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf yay/
+else
+    echo "yay já está clonado em ~/yay"
 fi
 
-echo "Atualizando o sistema..."
-sudo pacman -Syu --noconfirm
-
-echo "Atualização Concluída."
-
-echo "Instalndo git"
-sudo pacman -S git
-
-echo "Clonando repositório yay"
-git clone 
+sudo pacman -S hyprland hyprlock hypridle hyprcursor hyprpaper hyprpicker waybar kitty rofi-wayland dolphin dolphin-plugins ark kio-admin polkit-kde-agent qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-desktop-portal-gtk dunst cliphist mpv pavucontrol xdg-user-dirs-gtk ttf-font-awesome ttf-jetbrains-mono-nerd noto-fonts ttf-droid 
